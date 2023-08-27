8b2d11080fb07c7713067f3ed1d4012416db90ea
unknown <thanhdoan.dn127gmail.com>
2023-08-27 09:22:12 +0700

    Clean up

--- gulp/tasks/development/build-template.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp");
-
-var template = require("gulp-template-html");
-
-gulp.task("build-template", function () {
-  return gulp.src("./app/docs/pages/*.html")
-    .pipe(template("./app/docs/template/main.html"))
-    .pipe(gulp.dest("./dist/docs"));
-});
